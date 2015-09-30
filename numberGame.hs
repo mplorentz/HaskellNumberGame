48bc65b927e03a151a7fe04838db6bedfe3ba592
Matt Lorentz <matt@lorentz.is>
2015-09-29 21:07:50 -0400

    10 more lines removed pro skillz

--- numberGame.hs
+++ numberGame.hs
@@ -1,13 +1,10 @@
 import System.Random
-import Data.Maybe
-import Control.Monad   
 
 main = do
    randomNumberGenerator <- getStdGen
    let winningNumber = head (randomRs (1, 10) randomNumberGenerator :: [Int])
    playGame winningNumber
 
-playGame :: Int -> IO ()
 playGame winningNumber = do
    putStrLn "I've chosen a random number between 1 and 100. See if you can guess it!"
    runRound winningNumber
@@ -16,19 +13,13 @@ runRound winningNumber = do
    userString <- getLine
    let parsedString = reads userString :: [(Int, String)]
    let (outputString, nextAction) = processNumber parsedString winningNumber 
-   case outputString of 
-      Just output -> do
-         putStrLn output
-         nextAction
-      _ -> nextAction
+   putStrLn outputString
+   nextAction
 
-processNumber :: [(Int, String)] -> Int -> (Maybe String, IO ())
-processNumber [(userNumber, unparseable)] winningNumber
-   | null unparseable   = outputFromComparingNumbers userNumber winningNumber
-   | otherwise          = (Just "Please enter only numbers", runRound winningNumber)
+processNumber [(userNumber, [])] winningNumber = outputFromComparingNumbers userNumber winningNumber 
+processNumber _ winningNumber = ("Please enter only numbers", runRound winningNumber)
 
-outputFromComparingNumbers :: Int -> Int -> (Maybe String, IO ())
 outputFromComparingNumbers userNumber winningNumber
-   | userNumber < winningNumber = (Just "Too low", runRound winningNumber)
-   | userNumber > winningNumber = (Just "Too high", runRound winningNumber)
-   | otherwise = (Nothing, putStrLn "Congratulations! You guessed it!")
+   | userNumber < winningNumber = ("Too low", runRound winningNumber)
+   | userNumber > winningNumber = ("Too high", runRound winningNumber)
+   | otherwise = ("Congratulations! You guessed it!", return ())
